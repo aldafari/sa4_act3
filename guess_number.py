d0f7aec04b242e69e8892d3f67455ddfb8ab7858
aldafari <aldafari@memphis.edu>
2024-02-08 17:26:29 -0600

    Displayed number of tries

--- guess_number.py
+++ guess_number.py
@@ -9,13 +9,16 @@ if guess == number:
 elif guess == 0:
     print("Quiting already? The number was",number)
 else:
-    while guess != number and guess != 0:
-        print(f"Sorry! try again. Or enter 0 to quit.")
+    while guess != number and guess != 0 and limit > 1:
+        limit = limit -1
+        print(f"Sorry! try again. Or enter 0 to quit. You have",limit,"tries left.")
         guess = int(input())
+        
         if guess == number:
             print("Congrats! You guessed the right number.")
         elif guess == 0:
             print("Quiting already? The number was",number)
+        elif limit == 1:
+            print("Sorry you're out of tries. Number was 10.")
 if guess==10:
     print("Congrats! You guessed the right number.")
-#test
