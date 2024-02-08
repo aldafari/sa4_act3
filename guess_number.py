7f8328db21725a437c730f1529ad57e924f1e014
aldafari <aldafari@memphis.edu>
2024-02-08 17:12:40 -0600

    Limited number of guesses

--- guess_number.py
+++ guess_number.py
@@ -1,15 +1,19 @@
 number = 10
+limit = 5
 
 print("I'm thinking of a number...")
 guess = int(input("What number am I thinking of? "))
+
 if guess == number:
     print("Congrats! You guessed the right number.")
 elif guess == 0:
     print("Quiting already? The number was",number)
 else:
-    while guess != number and guess != 0:
-        print(f"Sorry! try again. Or enter 0 to quit.")
+    while guess != number and guess != 0 and limit != 0:
+        limit = limit -1 
+        print(f"Sorry! try again. Or enter 0 to quit. You have",limit,"left")
         guess = int(input())
+        
         if guess == number:
             print("Congrats! You guessed the right number.")
         elif guess == 0:
