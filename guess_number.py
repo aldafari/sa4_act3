986d32e41de5d8d34a8ebed8b30cf5cc8c8c6327
aldafari <aldafari@memphis.edu>
2024-02-08 16:44:28 -0600

    Added infinite guesses for user until they enter 0

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,19 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of?"))
-
+guess = int(input("What number am I thinking of? "))
 if guess == number:
     print("Congrats! You guessed the right number.")
+elif guess == 0:
+    print("Quiting already? The number was",number)
 else:
-    print(f"Sorry! the number was {number}.")
+    while guess != number and guess != 0:
+        print(f"Sorry! try again. Or enter 0 to quit.")
+        guess = int(input())
+        if guess == number:
+            print("Congrats! You guessed the right number.")
+        elif guess == 0:
+            print("Quiting already? The number was",number)
+        
+
     
