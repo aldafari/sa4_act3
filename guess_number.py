d6a429a65bb79943ec696c338af3b2f116bbe20f
aldafari <aldafari@memphis.edu>
2024-02-08 16:56:58 -0600

    Gave user hint if guess is too high or low

--- guess_number.py
+++ guess_number.py
@@ -8,12 +8,14 @@ elif guess == 0:
     print("Quiting already? The number was",number)
 else:
     while guess != number and guess != 0:
-        print(f"Sorry! try again. Or enter 0 to quit.")
-        guess = int(input())
-        if guess == number:
-            print("Congrats! You guessed the right number.")
+        
+        if guess < 10:
+            print(f"Sorry! Try a higher number or enter 0 to quit.")
+            guess = int(input())
+        elif guess > 10:
+            print(f"Sorry! Try a lower number or enter 0 to quit.")
+            guess = int(input())
         elif guess == 0:
             print("Quiting already? The number was",number)
-        
-
-    
+if guess==10:
+    print("Congrats! You guessed the right number.")
